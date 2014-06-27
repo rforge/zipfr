26048dfabbe52aeeebf33b549a851d69dbdeb716
schtepf <schtepf@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-06-27 09:33:47 +0000

    zipfR v0.6-7: small improvements to plot management
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/zipfr@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/zipfR/R/zipfR_plotutils.R
+++ pkg/zipfR/R/zipfR_plotutils.R
@@ -2,46 +2,70 @@ zipfR.begin.plot <- function (device=zipfR.par("device"), filename="",
                               width=zipfR.par("width"), height=zipfR.par("height"),
                               bg=zipfR.par("bg"), pointsize=zipfR.par("pointsize"))
 {
-  if (device %in% c("png", "eps", "pdf") && (missing(filename) || filename==""))
-    stop("'filename' is required for ",device," plot device")
+  on.screen <- device %in% c("x11", "quartz") # whether this is an on-screen device
+  if (!on.screen) {
+    if (device %in% c("png", "eps", "pdf")) {
+      if (missing(filename) || filename=="") stop("filename= is required for ",device," plot device")
+    }
+    else {
+      stop("unsupported plot device '",device,"'")
+    }
+  }
 
-  ## close currently open file plot (or x11/quartz device that's still active)
-  if (.PLOT$id > 0) dev.off(.PLOT$id) 
+  already.open <- FALSE # on-screen device may already be open
+ 
+  if (.PLOT$id > 0 && .PLOT$id %in% dev.list()) {
+    if (on.screen) {
+      if (device == .PLOT$config$device # we can reuse open screen device unless parameters have changed
+          && width == .PLOT$config$width
+          && height == .PLOT$config$height
+          && bg == .PLOT$config$bg
+          && pointsize == .PLOT$config$pointsize) {
+        dev.set(.PLOT$id) # in case user has switched to a different device
+        .PLOT$device <- device # indicate device is open
+        already.open <- TRUE
+      }
+    } else {
+      zipfR.end.plot() # close active file plot before starting new device
+    }
+  }
 
-  ## open new device of specified type with specified parameters
-  png.res <- 120                        # default resolution for PNG files is 150 dpi
-  switch(device,
-         x11 = X11(width=width, height=height, bg=bg, pointsize=pointsize),
-         quartz = {quartz(width=width, height=height, pointsize=pointsize); par(bg=bg)},
-         png = png(filename=paste(filename, "png", sep="."),
-           width=width*png.res, height=height*png.res, res=png.res, bg=bg, pointsize=pointsize),
-         eps = postscript(file=paste(filename, "eps", sep="."),
-           width=width, height=height, bg=bg, pointsize=pointsize,
-           onefile=FALSE, horizontal=FALSE, paper="special"),
-         pdf = pdf(file=paste(filename, "pdf", sep="."),
-           width=width, height=height, bg=bg, pointsize=pointsize,
-           onefile=FALSE, paper="special"))
+  if (!already.open) {
+    ## open new device of specified type with specified parameters
+    png.res <- 150                        # default resolution for PNG files is 150 dpi
+    switch(device,
+           x11 = X11(width=width, height=height, bg=bg, pointsize=pointsize),
+           quartz = quartz(width=width, height=height, bg=bg, pointsize=pointsize),
+           png = png(filename=paste(filename, "png", sep="."), width=width*png.res, height=height*png.res, res=png.res, bg=bg, pointsize=pointsize),
+           eps = postscript(file=paste(filename, "eps", sep="."), width=width, height=height, bg=bg, pointsize=pointsize, onefile=FALSE, horizontal=FALSE, paper="special"),
+           pdf = pdf(file=paste(filename, "pdf", sep="."), width=width, height=height, bg=bg, pointsize=pointsize, onefile=FALSE, paper="special")
+           )
 
-  ## record information about active device in private .PLOT environment
-  .PLOT$device <- device
-  .PLOT$id <- dev.cur()
-
+    ## record information about active device in private .PLOT environment
+    .PLOT$device <- device
+    .PLOT$id <- dev.cur()
+    .PLOT$config <- list(device=device, width=width, height=height, bg=bg, pointsize=pointsize)
+  }
+  
   ## initialize graphics parameters
   init.par <- zipfR.par("init.par")
   if (!is.null(init.par) && length(init.par) > 0) do.call(par, init.par)
+
+  invisible(.PLOT$id)
 }
 
-zipfR.end.plot <- function ()
+zipfR.end.plot <- function (shutdown=FALSE)
 {
   if (.PLOT$id <= 0 || .PLOT$device == "") stop("no graphics device active at the moment")
-  if (.PLOT$device %in% c("x11", "quartz")) {
-    ## don't close screen device when plot is finished (only when starting new plot)
+  if (.PLOT$device %in% c("x11", "quartz") && !shutdown) {
+    ## don't close screen device when plot is finished (only when starting new plot with different geometry)
     .PLOT$device <- ""
   }
   else {
     dev.off(.PLOT$id)
     .PLOT$id <- 0
     .PLOT$device <- ""
+    .PLOT$config <- list(device="", width=0, height=0, bg="", pointsize=0)
   }
 }
 
@@ -63,3 +87,4 @@ zipfR.pick.device <- function(args=commandArgs())
 
 .PLOT$id <- 0
 .PLOT$device <- ""
+.PLOT$config <- list(device="", width=0, height=0, bg="", pointsize=0)
