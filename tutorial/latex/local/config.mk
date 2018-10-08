e0323d4cf994c294b71656a79a2c2f11efc1cf23
schtepf <schtepf@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-10-08 09:33:18 +0000

    JAECS 2018 keynote lecture + corresponding updates to LREC tutorial
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/zipfr@45 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tutorial/latex/local/config.mk
+++ tutorial/latex/local/config.mk
@@ -3,7 +3,7 @@
 ##
 
 ## specify names of presentations here (with extension ".pdf")
-FILES := tutorial_lrec2018.pdf misc_berlin2018.pdf kallimachos_wuerzburg_2018.pdf
+FILES := tutorial_lrec2018.pdf misc_berlin2018.pdf kallimachos_wuerzburg_2018.pdf jaecs_tokyo2018.pdf
 
 ## directory where generated PDF documents can be installed
 INSTALL_DIR := ../pdf
