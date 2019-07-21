c872374129e53ea4e637057c2ea731c85f1403a5
schtepf <schtepf@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-07-21 20:07:07 +0000

    Robust confidence interval estimators factored out; confint() now returns data frame
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/zipfr@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/zipfR/R/bootstrap_confint.R
@@ -0,0 +1,58 @@
+bootstrap.confint <- function (x, level=0.95, method=c("normal", "mad", "empirical")) {
+  method <- match.arg(method)
+  .sig.level <- (1 - level) / 2  # one-sided significance level corresponding to selected confidence level
+  if (!(is.matrix(x) && is.numeric(x))) stop("x must be a numeric matrix")
+  replicates <- nrow(x)
+  
+  if (method == "empirical") {
+    n.outliers <- round(replicates * .sig.level) # number of "outliers" to drop from each tail
+    if (n.outliers < 3) stop("insufficient bootstrap data for confidence level=", level, " (need at least ", ceiling(3 / .sig.level)," replicates)")
+    .sig.level <- n.outliers / replicates # "true" significance level of the empirical interval
+  }
+  confint.labels <- c(sprintf("%g%%", 100 * c(.sig.level, 1 - .sig.level)), "center", "spread")
+  
+  ## confint.fnc takes a vector x and returns the four estimates (lower, upper, center, spread)
+  confint.fnc <- switch(
+    method,
+    ## estimates based on Gaussian distribution (mean + s.d.)
+    normal = function (x) {
+      .mean <- mean(x)
+      .sd <- sd(x)
+      .C <- -qnorm(.sig.level) # extension of confidence interval in s.d. (z-score)
+      c(.mean - .C * .sd, .mean + .C * .sd, .mean, .sd)
+    },
+    ## estimates based on median absolute deviation (median, MAD adusted to s.d.)
+    mad = function (x) {
+      .mid <- median(x)
+      y <- x - .mid
+      .right <- y >= 0  # data points above median
+      .left <- y < 0    # data points below median
+      .madR <- median(abs(y[.right])) # one-sided median absolute deviation
+      .madL <- median(abs(y[.left]))
+      .sdR <- .madR / qnorm(3/4) # robust MAD estimator for s.d. of Gaussian distribution
+      .sdL <- .madL / qnorm(3/4) # (cf. Wikipedia article "Median absolute deviation")
+      .C <- -qnorm(.sig.level) # extension of confidence interval in s.d. (z-score)
+      c(.mid - .C * .sdL, .mid + .C * .sdR, .mid, mean(.sdL, .sdR))
+    },
+    ## empirical confidence interval (median, IQR/2 adjusted to s.d.)
+    empirical = function (x) {
+      y <- sort(x)
+      c(y[n.outliers + 1], y[replicates - n.outliers], median(x), IQR(x) / (2 * qnorm(3/4)))
+    }
+  )
+  confint.wrap <- function (x) {
+    ## catch some special cases
+    if (!any(is.na(x)) && min(x) == Inf && max(x) == Inf) {
+      c(Inf, Inf, Inf, Inf) # special case for infinite population diversity
+    }
+    else if (any(!is.finite(x))) {
+      c(NA, NA, NA, NA) # if there are missing or infinite values
+    }
+    else confint.fnc(x)
+  }
+  
+  .res.table <- apply(x, 2, confint.wrap)
+  rownames(.res.table) <- confint.labels
+
+  .res.table
+}
--- pkg/zipfR/R/confint_lnre.R
+++ pkg/zipfR/R/confint_lnre.R
@@ -1,4 +1,4 @@
-confint.lnre <- function (object, parm, level=0.95, method=c("normal", "mad", "empirical"), plot=FALSE, breaks="Sturges", ...)
+confint.lnre <- function (object, parm, level=0.95, method=c("mad", "normal", "empirical"), plot=FALSE, breaks="Sturges", ...)
 { ## object should belong to class "lnre" if this method is called
   if (!("bootstrap" %in% names(object))) stop("no bootstrapping data available for LNRE model")
   replicates <- length(object$bootstrap)
@@ -31,62 +31,13 @@ confint.lnre <- function (object, parm, level=0.95, method=c("normal", "mad", "e
      .res
    })
    
-  .sig.level <- (1 - level) / 2  # one-sided significance level corresponding to selected confidence level
-  if (method == "empirical") {
-    n.outliers <- round(replicates * .sig.level) # number of "outliers" to drop from each tail
-    if (n.outliers < 3) stop("insufficient bootstrap data for confidence level=", level, " (need at least ",ceiling(3 / .sig.level)," replicates)")
-    .sig.level <- n.outliers / replicates # "true" significance level of the empirical interval
-  }
-  confint.labels <- c(sprintf("%g%%", 100 * c(.sig.level, 1 - .sig.level)), "center")
-
-  confint.fnc <- switch(method,
-    normal = function (x) {
-      if (min(x) == Inf && max(x) == Inf) {
-        c(Inf, Inf, Inf) # special case for infinite population diversity
-      }
-      else {
-        .mean <- mean(x)
-        .sd <- sd(x)
-        .C <- -qnorm(.sig.level) # extension of confidence interval in s.d. (z-score)
-        c(.mean - .C * .sd, .mean + .C * .sd, .mean)
-      }
-    },
-    mad = function (x) {
-      if (min(x) == Inf && max(x) == Inf) {
-        c(Inf, Inf, Inf) # special case for infinite population diversity
-      }
-      else {
-        .mid <- median(x)
-        y <- x - .mid
-        .right <- y >= 0  # data points above median
-        .left <- y < 0    # data points below median
-        .madR <- median(abs(y[.right])) # one-sided median absolute deviation
-        .madL <- median(abs(y[.left]))
-        .sdR <- .madR / qnorm(3/4) # robust MAD estimator for s.d. of Gaussian distribution
-        .sdL <- .madL / qnorm(3/4) # (cf. Wikipedia article "Median absolute deviation")
-        .C <- -qnorm(.sig.level) # extension of confidence interval in s.d. (z-score)
-        c(.mid - .C * .sdL, .mid + .C * .sdR, .mid)
-      }
-    },
-    empirical = function (x) {
-      if (min(x) == Inf && max(x) == Inf) {
-        c(Inf, Inf, Inf) # special case for infinite population diversity
-      }
-      else {
-        y <- sort(x)
-        c(y[n.outliers + 1], y[replicates - n.outliers], median(x))
-      }
-    }
-  )
-  
-  .res.table <- t(apply(do.call(rbind, .res.list), 2, confint.fnc))
-  rownames(.res.table) <- parm
-  colnames(.res.table) <- confint.labels
+  .res.table <- bootstrap.confint(do.call(rbind, .res.list), level=level, method=method)
+  colnames(.res.table) <- parm
 
   if (plot) {
     for (.p in parm) {
       x <- sapply(.res.list, function (.r) .r[.p])
-      .stats <- .res.table[.p, ]
+      .stats <- .res.table[, .p]
       .min <- min(x)
       .max <- max(x)
       if (.min > -Inf && .max < Inf) {
@@ -99,6 +50,6 @@ confint.lnre <- function (object, parm, level=0.95, method=c("normal", "mad", "e
     }
   }
 
-  .res.table
+  as.data.frame(.res.table)
 }
 
